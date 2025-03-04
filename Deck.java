b67dfe5a09665d23def43006b2af498fa185d79b
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-04 19:52:26 +0530

    method to compares two cards to see if they have the same rank

--- Deck.java
+++ Deck.java
@@ -27,7 +27,7 @@ class Deck {
 
     // Prints all cards in the deck
     public void printDeck() {
-        System.out.println("\n📜 Current Deck:");
+        System.out.println("\n Current Deck:");
         for (Card card : cardDeck) {
             System.out.println(card);
         }
@@ -36,18 +36,33 @@ class Deck {
     // Prints a specific card by index
     public void printCard(int index) {
         if (index >= 0 && index < cardDeck.size()) {
-            System.out.println("\n🔹 Selected Card: " + cardDeck.get(index));
+            System.out.println("\n Selected Card: " + cardDeck.get(index));
         } else {
-            System.out.println("\n❌ Invalid card index!");
+            System.out.println("\n Invalid card index!");
         }
     }
 
     // Finds and prints all cards of the same suit
     public void sameCard(String suit) {
-        System.out.println("\n📌 Cards with Suit: " + suit);
+        System.out.println("\n Cards with Suit: " + suit);
         for (Card card : cardDeck) {
             if (card.getSuit().equalsIgnoreCase(suit)) {
                 System.out.println(card);
             }
+        }
+    }
+
+    // Compares two cards to see if they have the same rank
+    public void compareCard(int index1, int index2) {
+        if (index1 >= 0 && index1 < cardDeck.size() && index2 >= 0 && index2 < cardDeck.size()) {
+            Card card1 = cardDeck.get(index1);
+            Card card2 = cardDeck.get(index2);
+            if (card1.getRank().equals(card2.getRank())) {
+                System.out.println("\n The cards have the same rank!");
+            } else {
+                System.out.println("\n The cards have different ranks.");
+            }
+        } else {
+            System.out.println("\n Invalid card indices!");
         }
     }
