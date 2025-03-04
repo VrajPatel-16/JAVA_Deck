d2dee41913e2060a9103a6147e708bc57f590efd
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-04 19:41:01 +0530

    Created a class Card

---
+++ Card.java
@@ -0,0 +1,12 @@
+// Card.java
+// Defines a Card class with rank and suit attributes
+
+class Card {
+    private String rank;
+    private String suit;
+
+    // Constructor to initialize a card
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
